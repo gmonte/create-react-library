41a6a17cac0067d046c9174c92a633e5b1ffefde
Guilherme Monte <guilherme.monte@syntesis.com.br>
2018-09-28 16:41:49 -0300

    changed to syntesis needs

--- template/default/example/src/App.js
+++ template/default/example/src/App.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react'
-
+import { JssProvider } from 'react-jss'
+import { create as createJss } from 'jss'
+import jssPreset from 'jss-preset-default'
+import { createGenerateClassName } from '@material-ui/core/styles'
+import CssBaseline from '@material-ui/core/CssBaseline'
 import ExampleComponent from '{{name}}'
 
+const jss = createJss(jssPreset())
+
+const generateClassName = createGenerateClassName()
+
 export default class App extends Component {
-  render () {
+  render() {
     return (
-      <div>
-        <ExampleComponent text='Modern React component module' />
-      </div>
+      <JssProvider
+        jss={ jss }
+        generateClassName={ generateClassName }
+      >
+        <div>
+          <CssBaseline />
+          <ExampleComponent />
+        </div>
+      </JssProvider>
     )
   }
 }
--- template/default/example/src/index.js
+++ template/default/example/src/index.js
@@ -1,7 +1,16 @@
-import React from 'react'
+import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
+import Loadable from 'react-loadable'
 
-import './index.css'
-import App from './App'
+const LoadableComponent = Loadable({
+  loader: () => import('./App'),
+  loading: () => <h3>Loading...</h3>
+})
+
+class App extends Component {
+  render() {
+    return <LoadableComponent />
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById('root'))
